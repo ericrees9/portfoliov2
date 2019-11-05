397704ada2066a4baa3be58b49e3a4cab911dffc
Eric Rees <erees@protonmail.com>
2019-11-05 11:15:30 -0500

    First commit.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is going to be my portfolio website soon!!!!
+          This is going to be my portfolio website soon!!!!!!!!
         </p>
       </header>
     </div>
